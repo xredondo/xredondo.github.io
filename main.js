f5e3259fe641b56b64c9c75bd7c65a4daad52d6b
xredondo <xredondo23@gmail.com>
2019-10-10 23:01:48 -0700

    updated saints home

--- main.js
+++ main.js
@@ -1,6 +1,6 @@
 $(window).scroll(function () {
 if ($(window).scrollTop() >= 50) {
-$('.navbar').css('background','red');
+$('.navbar').css('background','grey');
 } else {
 $('.navbar').css('background','transparent');
 }
