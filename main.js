81901b0542a91c692a809c5922f0851dd366e8f2
xredondo <xredondo23@gmail.com>
2019-10-14 21:56:41 -0700

    changed saints page

--- main.js
+++ main.js
@@ -1,7 +1,22 @@
+$(document).ready(function(){
+  $("button").click(function(){
+    var navlinksdiv = $('#divdropdown')
+    if (navlinksdiv.hasClass('hidden')){
+      navlinksdiv.removeClass('hidden');
+      $('#navTransitionId').addClass('opaqueNavbar');
+      $("button").addClass('clickedbutton')
+    } else {
+      navlinksdiv.addClass('hidden');
+      $('#navTransitionId').removeClass('opaqueNavbar');
+      $("button").removeClass('clickedbutton')
+    }
+  });
+});
+
 $(window).scroll(function () {
-if ($(window).scrollTop() >= 50) {
-$('.navbar').css('background','grey');
-} else {
-$('.navbar').css('background','transparent');
-}
+	if ($(window).scrollTop() >= 30) {
+		$('#navTransitionId').addClass('opaqueNavbar');
+	} else {
+		$('#navTransitionId').removeClass('opaqueNavbar');
+	}
 });
